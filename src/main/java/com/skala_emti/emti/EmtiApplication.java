acd3f2d163b4bc9e599c4dd2cca9da55897f3887
gwonsang <gwonsang247@gmail.com>
2025-03-09 23:37:14 +0900

    check:checking

--- src/main/java/com/skala_emti/emti/EmtiApplication.java
+++ src/main/java/com/skala_emti/emti/EmtiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EmtiApplication {
 //TEST
 //TEST
+//TEST
 	public static void main(String[] args) {
 		SpringApplication.run(EmtiApplication.class, args);
 	}
